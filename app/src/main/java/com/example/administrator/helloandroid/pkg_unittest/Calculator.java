0c52ff3c42ded272656cb7d72876104b2999cd09
hyunkiung <euroeaster@hanmail.net>
2015-05-20 09:45:53 +0900

    pkg_unittest - 프로그램 코드 테스트

--- app/src/main/java/com/example/administrator/helloandroid/pkg_unittest/Calculator.java
+++ app/src/main/java/com/example/administrator/helloandroid/pkg_unittest/Calculator.java
@@ -4,7 +4,14 @@ package com.example.administrator.helloandroid.pkg_unittest;
  * Created by Administrator on 2015-05-19.
  */
 public class Calculator {
+/*
+탐색기 모드를 Project로 바꾸고 폴더 생성
+app -> src -> test -> java
 
+아래 메소드에서 마우스 오른쪽 - Go To - Test
+그럼 생성한 폴더에 테스트 파일이 생성된다.
+
+ */
     public int sum(int a, int b) {
         return a + b;
     }
--- app/src/test/java/com/example/administrator/helloandroid/pkg_unittest/CalculatorTest.java
+++ app/src/test/java/com/example/administrator/helloandroid/pkg_unittest/CalculatorTest.java
@@ -2,21 +2,33 @@ package com.example.administrator.helloandroid.pkg_unittest;
 
 import junit.framework.TestCase;
 
+import org.junit.Assert;
+
 /**
  * Created by Administrator on 2015-05-19.
  */
 public class CalculatorTest extends TestCase {
 
+//    private static String TAG = "로그 / CalculatorTest ";
+//    private void show_Log(String msg) { Log.d(TAG, msg); }
+//    private String log;
+
     public void setUp() throws Exception {
         super.setUp();
-
+//        log = "테스트 시작 시 호출 되는 곳";
+//        show_Log(log);
     }
 
     public void tearDown() throws Exception {
-
+//        log = "테스트 종료 시 호출 되는 곳";
+//        show_Log(log);
     }
 
     public void testSum() throws Exception {
+        Calculator calc = new Calculator();
+        int result = calc.sum(1, 2);
 
+        Assert.assertEquals(result, 3);
+//        Assert.assertNotSame(result, 2);
     }
 }
